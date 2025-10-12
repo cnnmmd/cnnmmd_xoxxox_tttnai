7e94359c2cc21fa109d2386598baf1d564ba3764
azsgit <10238943+azsgit@users.noreply.github.com>
2025-10-12 20:37:45 +0900

    wip

--- export/prc/xoxxox/engine_tttnai.py
+++ export/prc/xoxxox/engine_tttnai.py
@@ -1,10 +1,9 @@
-import re
 from novelai_api.GlobalSettings import GlobalSettings
 from novelai_api.Preset import Model, Preset
 from novelai_api.Tokenizer import Tokenizer
 from novelai_api.utils import b64_to_tokens
 from xoxxox.naiapi.boilerplate import API
-from xoxxox.shared import Custom
+from xoxxox.shared import Custom, LibLog
 
 #---------------------------------------------------------------------------
 
@@ -20,61 +19,32 @@ class TttPrc:
     self.preset.min_length =  4
     self.preset.max_length = 20
     self.config = GlobalSettings()
+    self.conlog = {}
 
   def status(self, config="xoxxox/config_tttnai_000", **dicprm):
     diccnf = Custom.update(config, dicprm)
-    # 設定：全般
-    self.lsthed = []
-    self.lstbdy = []
-    self.frmsys = "{elmsys}\n"
-    self.frmusr = "{txtsrc}{txtdef}{elmusr}\n"
-    self.frmagt = "{txtdst}{txtdef}{elmagt}\n"
-    # 設定：個別
-    self.maxbdy = diccnf["prmmax"]
-    self.nulagt = diccnf["nuloth"]
-    self.dictlk = {
-      "elmsys": diccnf["status"],
-      "txtsrc": diccnf["rolslf"],
-      "txtdst": diccnf["roloth"],
-      "elmusr": diccnf["inislf"],
-      "elmagt": diccnf["inioth"],
-      "txtdef": "＞",
-    }
-    self.lsthed.append(self.frmsys.format_map(self.dictlk))
-    self.lstbdy.append(self.frmusr.format_map(self.dictlk))
-    self.lstbdy.append(self.frmagt.format_map(self.dictlk))
+    self.expert = diccnf["expert"]
+    if not (self.expert in self.conlog):
+      self.conlog[self.expert] = LibLog.getlog(diccnf["conlog"]) # LOG
+      self.conlog[self.expert].catsys(diccnf) # LOG
 
   async def infere(self, txtreq):
     async with API() as hdlapi:
       apinai = hdlapi.api
-      # 生成：導入
-      self.dictlk["elmusr"] = txtreq
-      self.lstbdy.append(self.frmusr.format_map(self.dictlk))
-      strlog = "".join(self.lsthed + self.lstbdy)
-      #print("strlog[" + strlog + "]", flush=True) # DBG
-      # 生成：推定
-      prompt = Tokenizer.encode(self.nmodel, strlog)
-      infenc = await apinai.high_level.generate(
-        prompt,
+      prompt = self.conlog[self.expert].catreq(txtreq) # LOG
+      print("prompt[", prompt, "]", sep="", flush=True) # DBG
+      tknprm = Tokenizer.encode(self.nmodel, prompt)
+      rawifr = await apinai.high_level.generate(
+        tknprm,
         self.nmodel,
         self.preset,
         self.config
       )
-      infdec = Tokenizer.decode(self.nmodel, b64_to_tokens(infenc["output"], self.nbytes))
-      #print("infdec[" + infdec + "]", flush=True) # DBG
-      # 結果：加工
-      try:
-        elmagt = re.findall(self.dictlk["txtdst"] + self.dictlk["txtdef"] + "(.*)", infdec)[0]
-      except Exception as e:
-        elmagt = ""
-      if elmagt == "":
-        elmagt = self.nulagt
-      # ログ：追加
-      self.dictlk["elmagt"] = elmagt
-      self.lstbdy.append(self.frmagt.format_map(self.dictlk))
-      if len(self.lstbdy) > self.maxbdy * 2:
-        self.lstbdy.pop(0)
-        self.lstbdy.pop(0)
-      # 結果：返却
-      txtres = elmagt
-      return (txtres, "")
+      #print("rawifr[", rawifr, "]", sep="", flush=True) # DBG
+      txtifr = Tokenizer.decode(self.nmodel, b64_to_tokens(rawifr["output"], self.nbytes))
+      print("txtifr[" + txtifr + "]", flush=True) # DBG
+      txtres, txtopt = self.conlog[self.expert].arrres(txtifr) # LOG
+      print("txtres[" + txtres + "]", flush=True) # DBG
+      print("txtopt[" + txtopt + "]", flush=True) # DBG
+      self.conlog[self.expert].catres(txtres) # LOG
+      return (txtres, txtopt)
